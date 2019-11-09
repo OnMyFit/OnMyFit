3c8b00cbecc5928f26faf1775075f7617b0bf869
woose28 <dldntmdsla@naver.com>
2019-11-09 15:09:41 +0900

    input data type 제한
    숫자 이외의 값이 들어올 경우 alert

--- cloth_drawing/Coat/create_Coat.js
+++ cloth_drawing/Coat/create_Coat.js
@@ -10,87 +10,91 @@ cloth.btn_create = document.getElementById("button_create");
 
 cloth.btn_create.addEventListener("click",function(){
     
-    cloth.data_shoulder = parseInt(document.getElementById("text_shoulder").value);
-    cloth.data_chest = parseInt(document.getElementById("text_chest").value);
-    cloth.data_sleeve = parseInt(document.getElementById("text_sleeve").value);
-    cloth.data_height = parseInt(document.getElementById("text_height").value);
+    cloth.data_shoulder = document.getElementById("text_shoulder").value;
+    cloth.data_chest = document.getElementById("text_chest").value;
+    cloth.data_sleeve = document.getElementById("text_sleeve").value;
+    cloth.data_height = document.getElementById("text_height").value;
     
-    var point_start_x = 100;
-    var point_start_y = 50;
-    var fixed_length = 3;
-
+    if(isNaN(cloth.data_shoulder)||isNaN(cloth.data_chest)||isNaN(cloth.data_sleeve)||isNaN(cloth.data_height)){
+        alert("숫자를 입력해주세요");
+    }else{
+        var point_start_x = 100;
+        var point_start_y = 50;
+        var fixed_length = 3;
     
-    d_path = "M "+point_start_x+" "+point_start_y;
-
+        
+        d_path = "M "+point_start_x+" "+point_start_y;
     
-    var point_shoulder_x = point_start_x+cloth.data_shoulder * 0.5;
-    var point_shoulder_y = point_start_y+5;
-    var point_shoulder_control_01_x = point_start_x+(cloth.data_shoulder*0.5*0.48);
-    var point_shoulder_control_01_y = point_start_y+1;
-    var point_shoulder_control_02_x = point_start_x+(cloth.data_shoulder*0.5*0.88);
-    var point_shoulder_control_02_y = point_start_y+2;
-
-    d_path = d_path+"C "+point_shoulder_control_01_x+" "+point_shoulder_control_01_y+" "+point_shoulder_control_02_x+" "+point_shoulder_control_02_y+" "+point_shoulder_x+" "+point_shoulder_y;
+        
+        var point_shoulder_x = point_start_x+cloth.data_shoulder * 0.5;
+        var point_shoulder_y = point_start_y+5;
+        var point_shoulder_control_01_x = point_start_x+(cloth.data_shoulder*0.5*0.48);
+        var point_shoulder_control_01_y = point_start_y+1;
+        var point_shoulder_control_02_x = point_start_x+(cloth.data_shoulder*0.5*0.88);
+        var point_shoulder_control_02_y = point_start_y+2;
     
-    var angle_arm_middle = 60;
-    var point_arm_middle_x = point_shoulder_x+parseFloat(cloth.data_sleeve*0.5*Math.cos(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length));
-    var point_arm_middle_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length));
-
-    var point_arm_control_01_x = point_shoulder_x+7;
-    var point_arm_control_01_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length))*0.3;
-
-    var point_arm_control_02_x = point_arm_middle_x-2;
-    var point_arm_control_02_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length))*0.6;
-
-    d_path = d_path+"C "+point_arm_control_01_x+" "+point_arm_control_01_y+" "+point_arm_control_02_x+" "+point_arm_control_02_y+" "+point_arm_middle_x+" "+point_arm_middle_y;
-
-    var angle_arm_bottom = 80;
-    var point_arm_bottom_x = point_arm_middle_x+parseFloat((cloth.data_sleeve*0.5+10)*Math.cos(angle_arm_bottom*(Math.PI/180)).toFixed(fixed_length));
-    var point_arm_bottom_y = point_arm_middle_y+parseFloat((cloth.data_sleeve*0.5+10)*Math.sin(angle_arm_bottom*(Math.PI/180)).toFixed(fixed_length));
-
-    d_path = d_path+"L "+point_arm_bottom_x+" "+point_arm_bottom_y;
-
-    var angle_sleeve = 10;
-    var point_sleeve_bottom_x = point_arm_bottom_x-parseFloat(10*Math.cos(angle_sleeve*(Math.PI/180)).toFixed(fixed_length));
-    var point_sleeve_bottom_y = point_arm_bottom_y+parseFloat(10*Math.sin(angle_sleeve*(Math.PI/180)).toFixed(fixed_length));
-
-    d_path = d_path+"L "+point_sleeve_bottom_x+" "+point_sleeve_bottom_y;
-
-    var point_cloth_bottom_center_x = point_start_x;
-    var point_cloth_bottom_center_y = point_start_y+cloth.data_height;
-
-    d_path = d_path+"M "+point_cloth_bottom_center_x+" "+point_cloth_bottom_center_y;
-
-    var point_cloth_bottom_right_x = point_cloth_bottom_center_x+cloth.data_chest*0.5+1;
-    var point_cloth_bottom_right_y = point_cloth_bottom_center_y-3;
-
-    var point_cloth_bottom_control_01_x = point_cloth_bottom_center_x+cloth.data_chest*0.5*0.3;
-    var point_cloth_bottom_control_01_y = point_cloth_bottom_center_y;
-    var point_cloth_bottom_control_02_x = point_cloth_bottom_center_x+cloth.data_chest*0.5*0.7;
-    var point_cloth_bottom_control_02_y = point_cloth_bottom_center_y;
-
-    d_path = d_path+"C "+point_cloth_bottom_control_01_x+" "+point_cloth_bottom_control_01_y+" "+point_cloth_bottom_control_02_x+" "+point_cloth_bottom_control_02_y+" "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
-
-    var point_chest_right_x = point_start_x+cloth.data_chest*0.5;
-    var point_chest_right_y = point_start_y+40;
-
-    var point_chest_control_01_x = point_chest_right_x-2;
-    var point_chest_control_01_y = point_chest_right_y+50;
-
-    var point_chest_control_02_x = point_chest_right_x-4;
-    var point_chest_control_02_y = point_chest_right_y+25;
-
-    d_path = d_path+"C "+point_chest_control_01_x+" "+point_chest_control_01_y+" "+point_chest_control_02_x+" "+point_chest_control_02_y+" "+point_chest_right_x+" "+point_chest_right_y;
-
-    d_path = d_path+"L "+point_sleeve_bottom_x+" "+point_sleeve_bottom_y;
-
-    //d_path = d_path+"M "+point_shoulder_x+" "+point_shoulder_y;
-    //d_path = d_path+"L "+point_chest_right_x+" "+point_chest_right_y;
-
-    document.getElementById("path_01").setAttribute("d", d_path);
-    document.getElementById("path_01").setAttribute("stroke-width", "0.5");
-    document.getElementById("path_02").setAttribute("d", d_path);
-    document.getElementById("path_02").setAttribute("stroke-width", "0.5");
+        d_path = d_path+"C "+point_shoulder_control_01_x+" "+point_shoulder_control_01_y+" "+point_shoulder_control_02_x+" "+point_shoulder_control_02_y+" "+point_shoulder_x+" "+point_shoulder_y;
+        
+        var angle_arm_middle = 60;
+        var point_arm_middle_x = point_shoulder_x+parseFloat(cloth.data_sleeve*0.5*Math.cos(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length));
+        var point_arm_middle_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length));
     
-    document.getElementById("textarea_01").value = d_path;
+        var point_arm_control_01_x = point_shoulder_x+7;
+        var point_arm_control_01_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length))*0.3;
+    
+        var point_arm_control_02_x = point_arm_middle_x-2;
+        var point_arm_control_02_y = point_shoulder_y+parseFloat(cloth.data_sleeve*0.5*Math.sin(angle_arm_middle*(Math.PI/180)).toFixed(fixed_length))*0.6;
+    
+        d_path = d_path+"C "+point_arm_control_01_x+" "+point_arm_control_01_y+" "+point_arm_control_02_x+" "+point_arm_control_02_y+" "+point_arm_middle_x+" "+point_arm_middle_y;
+    
+        var angle_arm_bottom = 80;
+        var point_arm_bottom_x = point_arm_middle_x+parseFloat((cloth.data_sleeve*0.5+10)*Math.cos(angle_arm_bottom*(Math.PI/180)).toFixed(fixed_length));
+        var point_arm_bottom_y = point_arm_middle_y+parseFloat((cloth.data_sleeve*0.5+10)*Math.sin(angle_arm_bottom*(Math.PI/180)).toFixed(fixed_length));
+    
+        d_path = d_path+"L "+point_arm_bottom_x+" "+point_arm_bottom_y;
+    
+        var angle_sleeve = 10;
+        var point_sleeve_bottom_x = point_arm_bottom_x-parseFloat(10*Math.cos(angle_sleeve*(Math.PI/180)).toFixed(fixed_length));
+        var point_sleeve_bottom_y = point_arm_bottom_y+parseFloat(10*Math.sin(angle_sleeve*(Math.PI/180)).toFixed(fixed_length));
+    
+        d_path = d_path+"L "+point_sleeve_bottom_x+" "+point_sleeve_bottom_y;
+    
+        var point_cloth_bottom_center_x = point_start_x;
+        var point_cloth_bottom_center_y = point_start_y+cloth.data_height*1;
+    
+        d_path = d_path+"M "+point_cloth_bottom_center_x+" "+point_cloth_bottom_center_y;
+    
+        var point_cloth_bottom_right_x = point_cloth_bottom_center_x+cloth.data_chest*0.5+1;
+        var point_cloth_bottom_right_y = point_cloth_bottom_center_y-3;
+    
+        var point_cloth_bottom_control_01_x = point_cloth_bottom_center_x+cloth.data_chest*0.5*0.3;
+        var point_cloth_bottom_control_01_y = point_cloth_bottom_center_y;
+        var point_cloth_bottom_control_02_x = point_cloth_bottom_center_x+cloth.data_chest*0.5*0.7;
+        var point_cloth_bottom_control_02_y = point_cloth_bottom_center_y;
+    
+        d_path = d_path+"C "+point_cloth_bottom_control_01_x+" "+point_cloth_bottom_control_01_y+" "+point_cloth_bottom_control_02_x+" "+point_cloth_bottom_control_02_y+" "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
+    
+        var point_chest_right_x = point_start_x+cloth.data_chest*0.5;
+        var point_chest_right_y = point_start_y+40;
+    
+        var point_chest_control_01_x = point_chest_right_x-2;
+        var point_chest_control_01_y = point_chest_right_y+50;
+    
+        var point_chest_control_02_x = point_chest_right_x-4;
+        var point_chest_control_02_y = point_chest_right_y+25;
+    
+        d_path = d_path+"C "+point_chest_control_01_x+" "+point_chest_control_01_y+" "+point_chest_control_02_x+" "+point_chest_control_02_y+" "+point_chest_right_x+" "+point_chest_right_y;
+    
+        d_path = d_path+"L "+point_sleeve_bottom_x+" "+point_sleeve_bottom_y;
+    
+        //d_path = d_path+"M "+point_shoulder_x+" "+point_shoulder_y;
+        //d_path = d_path+"L "+point_chest_right_x+" "+point_chest_right_y;
+    
+        document.getElementById("path_01").setAttribute("d", d_path);
+        document.getElementById("path_01").setAttribute("stroke-width", "0.5");
+        document.getElementById("path_02").setAttribute("d", d_path);
+        document.getElementById("path_02").setAttribute("stroke-width", "0.5");
+        
+        document.getElementById("textarea_01").value = d_path;
+    }
 })
--- cloth_drawing/Pants/create_Pants.js
+++ cloth_drawing/Pants/create_Pants.js
@@ -11,140 +11,144 @@ cloth.btn_create = document.getElementById("button_create");
 
 cloth.btn_create.addEventListener("click",function(){
 
-    cloth.data_height = parseInt(document.getElementById("text_height").value);
-    cloth.data_waist = parseInt(document.getElementById("text_waist").value);
-    cloth.data_thigh = parseInt(document.getElementById("text_thigh").value);
-    cloth.data_under = parseInt(document.getElementById("text_under").value);
-    cloth.data_edge = parseInt(document.getElementById("text_edge").value);
+    cloth.data_height = document.getElementById("text_height").value;
+    cloth.data_waist = document.getElementById("text_waist").value;
+    cloth.data_thigh = document.getElementById("text_thigh").value;
+    cloth.data_under = document.getElementById("text_under").value;
+    cloth.data_edge = document.getElementById("text_edge").value;
     
-    var point_start_x = 100;
-    var point_start_y = 50;
-    var fixed_length = 3;
-
+    if(isNaN(cloth.data_height)||isNaN(cloth.data_waist)||isNaN(cloth.data_thigh)||isNaN(cloth.data_under)||isNaN(cloth.data_edge)){
+        alert("숫자를 입력해주세요");
+    }else{
+        var point_start_x = 100;
+        var point_start_y = 50;
+        var fixed_length = 3;
     
-    //바지 옆모습
-    //바지 옆모습 = d_path
-    //바지 앞모습 = d_path_front
-    d_path = "M "+point_start_x+" "+point_start_y;
-
-    var point_waist_top_x = point_start_x + cloth.data_waist*0.5;
-    var point_waist_top_y = point_start_y;
-
-    d_path = d_path+"L "+point_waist_top_x+" "+point_waist_top_y;
-
-    var point_waist_bottom_x = point_waist_top_x+1;
-    var point_waist_bottom_y = point_waist_top_y+5;
-
-    d_path = d_path+"L "+point_waist_bottom_x+" "+point_waist_bottom_y;
-
-    var point_waist_bottom_to_center_x = point_start_x;
-    var point_waist_bottom_to_center_y = point_waist_bottom_y;
-
-    d_path = d_path+"L "+point_waist_bottom_to_center_x+" "+point_waist_bottom_to_center_y;
-
-    d_path = d_path+"M "+point_start_x+" "+point_start_y;
-
-    var point_cloth_bottom_center_x = point_start_x;
-    var point_cloth_bottom_center_y = point_start_y+cloth.data_height;
-
-    d_path = d_path+"L "+point_cloth_bottom_center_x+" "+point_cloth_bottom_center_y;
-
-    var point_cloth_bottom_right_x = point_cloth_bottom_center_x+cloth.data_edge;
-    var point_cloth_bottom_right_y = point_cloth_bottom_center_y;
-
-    d_path = d_path+"L "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
-
-    d_path = d_path+"M "+point_waist_bottom_x+" "+point_waist_bottom_y;
-
-    var point_thigh_x = point_start_x+cloth.data_thigh;
-    var point_thigh_y = point_start_y+cloth.data_under;
-
-    var point_thigh_control_01_x = point_thigh_x - 7;
-    var point_thigh_control_01_y = point_thigh_y - 9;
-
-    var point_thigh_control_02_x = point_thigh_x - 5;
-    var point_thigh_control_02_y = point_thigh_y - 4;
-
-    d_path = d_path+"C "+point_thigh_control_01_x+" "+point_thigh_control_01_y+" "+point_thigh_control_02_x+" "+point_thigh_control_02_y+" "+point_thigh_x+" "+point_thigh_y;
+        
+        //바지 옆모습
+        //바지 옆모습 = d_path
+        //바지 앞모습 = d_path_front
+        d_path = "M "+point_start_x+" "+point_start_y;
     
-    d_path = d_path+"L "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
+        var point_waist_top_x = point_start_x + cloth.data_waist*0.5;
+        var point_waist_top_y = point_start_y;
     
-
-    //-------------------------------------------------------------------------------------------------------
-    //여기서 부터 바지 앞 모습
-    d_path_front = "M "+point_start_x+" "+point_start_y;
-    var point_front_waist_top_x = point_start_x+cloth.data_waist*0.5;
-    var point_front_waist_top_y = point_start_y;
-
-    d_path_front = d_path_front+"L "+point_front_waist_top_x+" "+point_front_waist_top_y;
-
-    var point_front_waist_bottom_x = point_front_waist_top_x+1;
-    var point_front_waist_bottom_y = point_front_waist_top_y+5;
-
-    d_path_front = d_path_front+"L "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
-    d_path_front = d_path_front+"M "+point_front_waist_top_x+" "+point_front_waist_top_y;
-
-    var point_front_waist_to_center_top_x = point_start_x;
-    var point_front_waist_to_center_top_y = point_start_y+3;
-
-    var point_front_waist_to_center_top_control_01_x = point_start_x+cloth.data_waist*0.5*0.6;
-    var point_front_waist_to_center_top_control_01_y = point_start_y+3;
-    var point_front_waist_to_center_top_control_02_x = point_start_x+cloth.data_waist*0.5*0.3;
-    var point_front_waist_to_center_top_control_02_y = point_start_y+3;
-
-    d_path_front = d_path_front+"C "+point_front_waist_to_center_top_control_01_x+" "+point_front_waist_to_center_top_control_01_y+" "+point_front_waist_to_center_top_control_02_x+" "+point_front_waist_to_center_top_control_02_y+" "+point_front_waist_to_center_top_x+" "+point_front_waist_to_center_top_y;
-
-    var point_front_waist_to_center_bottom_x = point_start_x;
-    var point_front_waist_to_center_bottom_y = point_start_y+8;
-
-    var point_front_waist_to_center_bottom_control_01_x = point_start_x+cloth.data_waist*0.5*0.6;
-    var point_front_waist_to_center_bottom_control_01_y = point_start_y+8;
-    var point_front_waist_to_center_bottom_control_02_x = point_start_x+cloth.data_waist*0.5*0.3;
-    var point_front_waist_to_center_bottom_control_02_y = point_start_y+8;
-
-    d_path_front = d_path_front+"M "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
-    d_path_front = d_path_front+"C "+point_front_waist_to_center_bottom_control_01_x+" "+point_front_waist_to_center_bottom_control_01_y+" "+point_front_waist_to_center_bottom_control_02_x+" "+point_front_waist_to_center_bottom_control_02_y+" "+point_front_waist_to_center_bottom_x+" "+point_front_waist_to_center_bottom_y;
-
-    var point_front_under_x = point_start_x;
-    var point_front_under_y = point_front_waist_to_center_top_y+cloth.data_under;
-
-    d_path_front = d_path_front+"L "+point_front_under_x+" "+point_front_under_y;
-
-    var point_front_cloth_bottom_center_x = point_front_under_x+parseFloat(10*Math.tan(5*(Math.PI/180)).toFixed(fixed_length));
-    var point_front_cloth_bottom_center_y = point_front_under_y+10;
-
-    d_path_front = d_path_front+"L "+point_front_cloth_bottom_center_x+" "+point_front_cloth_bottom_center_y;
+        d_path = d_path+"L "+point_waist_top_x+" "+point_waist_top_y;
     
-    var point_front_cloth_bottom_right_x = point_start_x+cloth.data_waist*0.5+3;
-    var point_front_cloth_bottom_right_y = point_front_cloth_bottom_center_y;
-
-    d_path_front = d_path_front+"L "+point_front_cloth_bottom_right_x+" "+point_front_cloth_bottom_right_y;
+        var point_waist_bottom_x = point_waist_top_x+1;
+        var point_waist_bottom_y = point_waist_top_y+5;
     
-    d_path_front = d_path_front+"M "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
-
-    var point_front_thigh_x = point_start_x+cloth.data_waist*0.5+5;
-    var point_front_thigh_y = point_front_under_y-5;
-
-    var point_front_waist_to_thigh_control_01_x = point_front_thigh_x;
-    var point_front_waist_to_thigh_control_01_y = point_front_thigh_y-4;
-    var point_front_waist_to_thigh_control_02_x = point_front_thigh_x;
-    var point_front_waist_to_thigh_control_02_y = point_front_thigh_y-2;
-
-    d_path_front = d_path_front+"C "+point_front_waist_to_thigh_control_01_x+" "+point_front_waist_to_thigh_control_01_y+" "+point_front_waist_to_thigh_control_02_x+" "+point_front_waist_to_thigh_control_02_y+" "+point_front_thigh_x+" "+point_front_thigh_y
-    var point_front_thigh_to_bottom_control_01_x = point_front_thigh_x;
-    var point_front_thigh_to_bottom_control_01_y = point_front_thigh_y+2;
-    var point_front_thigh_to_bottom_control_02_x = point_front_thigh_x;
-    var point_front_thigh_to_bottom_control_02_y = point_front_thigh_y+4;
-
-    d_path_front = d_path_front+"C "+point_front_thigh_to_bottom_control_01_x+" "+point_front_thigh_to_bottom_control_01_y+" "+point_front_thigh_to_bottom_control_02_x+" "+point_front_thigh_to_bottom_control_02_y+" "+point_front_cloth_bottom_right_x+" "+point_front_cloth_bottom_right_y;
-
-    document.getElementById("path_01").setAttribute("d", d_path);
-    document.getElementById("path_01").setAttribute("stroke-width", "0.5");
+        d_path = d_path+"L "+point_waist_bottom_x+" "+point_waist_bottom_y;
     
-    document.getElementById("path_02").setAttribute("d", d_path_front);
-    document.getElementById("path_02").setAttribute("stroke-width", "0.5");
-    document.getElementById("path_03").setAttribute("d", d_path_front);
-    document.getElementById("path_03").setAttribute("stroke-width", "0.5");
-
-    document.getElementById("textarea_01").value = d_path;
+        var point_waist_bottom_to_center_x = point_start_x;
+        var point_waist_bottom_to_center_y = point_waist_bottom_y;
+    
+        d_path = d_path+"L "+point_waist_bottom_to_center_x+" "+point_waist_bottom_to_center_y;
+    
+        d_path = d_path+"M "+point_start_x+" "+point_start_y;
+    
+        var point_cloth_bottom_center_x = point_start_x;
+        var point_cloth_bottom_center_y = point_start_y+cloth.data_height*1;
+    
+        d_path = d_path+"L "+point_cloth_bottom_center_x+" "+point_cloth_bottom_center_y;
+    
+        var point_cloth_bottom_right_x = point_cloth_bottom_center_x+cloth.data_edge*1;
+        var point_cloth_bottom_right_y = point_cloth_bottom_center_y;
+    
+        d_path = d_path+"L "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
+    
+        d_path = d_path+"M "+point_waist_bottom_x+" "+point_waist_bottom_y;
+    
+        var point_thigh_x = point_start_x+cloth.data_thigh*1;
+        var point_thigh_y = point_start_y+cloth.data_under*1;
+    
+        var point_thigh_control_01_x = point_thigh_x - 7;
+        var point_thigh_control_01_y = point_thigh_y - 9;
+    
+        var point_thigh_control_02_x = point_thigh_x - 5;
+        var point_thigh_control_02_y = point_thigh_y - 4;
+    
+        d_path = d_path+"C "+point_thigh_control_01_x+" "+point_thigh_control_01_y+" "+point_thigh_control_02_x+" "+point_thigh_control_02_y+" "+point_thigh_x+" "+point_thigh_y;
+        
+        d_path = d_path+"L "+point_cloth_bottom_right_x+" "+point_cloth_bottom_right_y;
+        
+    
+        //-------------------------------------------------------------------------------------------------------
+        //여기서 부터 바지 앞 모습
+        d_path_front = "M "+point_start_x+" "+point_start_y;
+        var point_front_waist_top_x = point_start_x+cloth.data_waist*0.5;
+        var point_front_waist_top_y = point_start_y;
+    
+        d_path_front = d_path_front+"L "+point_front_waist_top_x+" "+point_front_waist_top_y;
+    
+        var point_front_waist_bottom_x = point_front_waist_top_x+1;
+        var point_front_waist_bottom_y = point_front_waist_top_y+5;
+    
+        d_path_front = d_path_front+"L "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
+        d_path_front = d_path_front+"M "+point_front_waist_top_x+" "+point_front_waist_top_y;
+    
+        var point_front_waist_to_center_top_x = point_start_x;
+        var point_front_waist_to_center_top_y = point_start_y+3;
+    
+        var point_front_waist_to_center_top_control_01_x = point_start_x+cloth.data_waist*0.5*0.6;
+        var point_front_waist_to_center_top_control_01_y = point_start_y+3;
+        var point_front_waist_to_center_top_control_02_x = point_start_x+cloth.data_waist*0.5*0.3;
+        var point_front_waist_to_center_top_control_02_y = point_start_y+3;
+    
+        d_path_front = d_path_front+"C "+point_front_waist_to_center_top_control_01_x+" "+point_front_waist_to_center_top_control_01_y+" "+point_front_waist_to_center_top_control_02_x+" "+point_front_waist_to_center_top_control_02_y+" "+point_front_waist_to_center_top_x+" "+point_front_waist_to_center_top_y;
+    
+        var point_front_waist_to_center_bottom_x = point_start_x;
+        var point_front_waist_to_center_bottom_y = point_start_y+8;
+    
+        var point_front_waist_to_center_bottom_control_01_x = point_start_x+cloth.data_waist*0.5*0.6;
+        var point_front_waist_to_center_bottom_control_01_y = point_start_y+8;
+        var point_front_waist_to_center_bottom_control_02_x = point_start_x+cloth.data_waist*0.5*0.3;
+        var point_front_waist_to_center_bottom_control_02_y = point_start_y+8;
+    
+        d_path_front = d_path_front+"M "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
+        d_path_front = d_path_front+"C "+point_front_waist_to_center_bottom_control_01_x+" "+point_front_waist_to_center_bottom_control_01_y+" "+point_front_waist_to_center_bottom_control_02_x+" "+point_front_waist_to_center_bottom_control_02_y+" "+point_front_waist_to_center_bottom_x+" "+point_front_waist_to_center_bottom_y;
+    
+        var point_front_under_x = point_start_x;
+        var point_front_under_y = point_front_waist_to_center_top_y+cloth.data_under*1;
+    
+        d_path_front = d_path_front+"L "+point_front_under_x+" "+point_front_under_y;
+    
+        var point_front_cloth_bottom_center_x = point_front_under_x+parseFloat(10*Math.tan(5*(Math.PI/180)).toFixed(fixed_length));
+        var point_front_cloth_bottom_center_y = point_front_under_y+10;
+    
+        d_path_front = d_path_front+"L "+point_front_cloth_bottom_center_x+" "+point_front_cloth_bottom_center_y;
+        
+        var point_front_cloth_bottom_right_x = point_start_x+cloth.data_waist*0.5+3;
+        var point_front_cloth_bottom_right_y = point_front_cloth_bottom_center_y;
+    
+        //d_path_front = d_path_front+"L "+point_front_cloth_bottom_right_x+" "+point_front_cloth_bottom_right_y;
+        
+        d_path_front = d_path_front+"M "+point_front_waist_bottom_x+" "+point_front_waist_bottom_y;
+    
+        var point_front_thigh_x = point_start_x+cloth.data_waist*0.5+5;
+        var point_front_thigh_y = point_front_under_y-5;
+    
+        var point_front_waist_to_thigh_control_01_x = point_front_thigh_x;
+        var point_front_waist_to_thigh_control_01_y = point_front_thigh_y-4;
+        var point_front_waist_to_thigh_control_02_x = point_front_thigh_x;
+        var point_front_waist_to_thigh_control_02_y = point_front_thigh_y-2;
+    
+        d_path_front = d_path_front+"C "+point_front_waist_to_thigh_control_01_x+" "+point_front_waist_to_thigh_control_01_y+" "+point_front_waist_to_thigh_control_02_x+" "+point_front_waist_to_thigh_control_02_y+" "+point_front_thigh_x+" "+point_front_thigh_y
+        var point_front_thigh_to_bottom_control_01_x = point_front_thigh_x;
+        var point_front_thigh_to_bottom_control_01_y = point_front_thigh_y+2;
+        var point_front_thigh_to_bottom_control_02_x = point_front_thigh_x;
+        var point_front_thigh_to_bottom_control_02_y = point_front_thigh_y+4;
+    
+        d_path_front = d_path_front+"C "+point_front_thigh_to_bottom_control_01_x+" "+point_front_thigh_to_bottom_control_01_y+" "+point_front_thigh_to_bottom_control_02_x+" "+point_front_thigh_to_bottom_control_02_y+" "+point_front_cloth_bottom_right_x+" "+point_front_cloth_bottom_right_y;
+    
+        document.getElementById("path_01").setAttribute("d", d_path);
+        document.getElementById("path_01").setAttribute("stroke-width", "0.5");
+        
+        document.getElementById("path_02").setAttribute("d", d_path_front);
+        document.getElementById("path_02").setAttribute("stroke-width", "0.5");
+        document.getElementById("path_03").setAttribute("d", d_path_front);
+        document.getElementById("path_03").setAttribute("stroke-width", "0.5");
+    
+        document.getElementById("textarea_01").value = d_path;
+    }
 })
