2e4b1a0ff585268396bb85bbdb588e760910a687
Jungsoo <bjsoo1123@naver.com>
2019-11-09 12:26:40 +0900

    error

--- cloth_drawing/Tshirt_short/create_Tshirt_short.js
+++ cloth_drawing/Tshirt_short/create_Tshirt_short.js
@@ -9,17 +9,17 @@ var cloth = {
 cloth.btn_create = document.getElementById("button_create");
 
 cloth.btn_create.addEventListener("click",function(){
-    
+
     cloth.data_shoulder = parseInt(document.getElementById("text_shoulder").value);
     cloth.data_chest = parseInt(document.getElementById("text_chest").value);
     cloth.data_sleeve = parseInt(document.getElementById("text_sleeve").value);
     cloth.data_height = parseInt(document.getElementById("text_height").value);
-    
-    var point_start_x = 100;
-    var point_start_y = 50;
+
+    var point_start_x = 49.8;
+    var point_start_y = 15;
     var fixed_length = 3;
 
-    
+
     d_path = "M "+point_start_x+" "+point_start_y;
 
     var point_neck_bottom_x = point_start_x;
@@ -34,7 +34,7 @@ cloth.btn_create.addEventListener("click",function(){
 
     d_path = d_path+"M "+point_neck_bottom_x+" "+point_neck_bottom_y;
     d_path = d_path+"C "+point_neck_control_01_x+" "+point_neck_control_01_y+" "+point_neck_control_02_x+" "+point_neck_control_02_y+" "+point_neck_right_x+" "+point_neck_right_y;
-    
+
     var point_shoulder_x = point_start_x+cloth.data_shoulder * 0.5;
     var point_shoulder_y = point_start_y+5;
     var point_shoulder_control_01_x = point_start_x+(cloth.data_shoulder*0.5*0.48);
@@ -43,14 +43,14 @@ cloth.btn_create.addEventListener("click",function(){
     var point_shoulder_control_02_y = point_start_y+2;
 
     d_path = d_path+"C "+point_shoulder_control_01_x+" "+point_shoulder_control_01_y+" "+point_shoulder_control_02_x+" "+point_shoulder_control_02_y+" "+point_shoulder_x+" "+point_shoulder_y;
-    
+
     var angle_sleeve_top = 40;
     var point_sleeve_top_x = point_shoulder_x+cloth.data_sleeve*parseFloat((Math.cos(angle_sleeve_top*(Math.PI/180))).toFixed(fixed_length));
     var point_sleeve_top_y = point_shoulder_y+cloth.data_sleeve*parseFloat((Math.sin(angle_sleeve_top*(Math.PI/180))).toFixed(fixed_length));
 
     d_path = d_path+"L "+point_sleeve_top_x+" "+point_sleeve_top_y;
 
-    var length_sleeve = 20; 
+    var length_sleeve = 20;
     var angle_sleeve_bottom = 67;
     var point_sleeve_bottom_x = point_sleeve_top_x-length_sleeve*parseFloat((Math.cos(angle_sleeve_bottom*(Math.PI/180))).toFixed(fixed_length));
     var point_sleeve_bottom_y = point_sleeve_top_y+length_sleeve*parseFloat((Math.sin(angle_sleeve_bottom*(Math.PI/180))).toFixed(fixed_length));
@@ -92,6 +92,6 @@ cloth.btn_create.addEventListener("click",function(){
     document.getElementById("path_01").setAttribute("stroke-width", "0.5");
     document.getElementById("path_02").setAttribute("d", d_path);
     document.getElementById("path_02").setAttribute("stroke-width", "0.5");
-    
+
     document.getElementById("textarea_01").value = d_path;
 })
---
+++ db.js
@@ -0,0 +1,141 @@
+
+//DB생성
+var db;
+var doneDB;
+
+function createDB() {
+  db = window.openDatabase("OnMyFit", "1.0", "테스트용DB", 4 * 1024 * 1024);
+}
+
+//Table 생성
+function createTopTable() {
+  db.transaction(function(tx) {
+    tx.executeSql("create table Top(top_name, top_shoulder,top_chest,top_arm,top_height)");
+  });
+}
+
+function createBottomTable() {
+  db.transaction(function(tx) {
+    tx.executeSql("create table Bottom(bottom_name,bottom_height,bottom_waist,bottom_thigh,bottom_under,bottom_edge)");
+  });
+}
+
+function createSkirtTable() {
+  db.transaction(function(tx) {
+    tx.executeSql("create table Skirt(skirt_name,skirt_height,skirt_waist,skirt_edge)");
+  });
+}
+
+function createOnepieceTable() {
+  db.transaction(function(tx) {
+    tx.executeSql("create table Onepiece(onpiece_name,onepiece_shoulder,onepiece_Chest,onepiece_arm,onepiece_heigth)");
+  });
+}
+
+function createLongDress() {
+  db.transaction(function(tx) {
+    tx.executeSql("create table LongDress(LongDress_name,LongDress_shoulder,LongDress_Chest,LongDress_arm,LongDress_height,LongDress_waist,LongDress_edge)");
+  });
+}
+
+function isCreate() {
+  if (!doneDB) {
+    doneDB = true;
+    create();
+    createTopTable();
+    createBottomTable();
+    createSkirtTable();
+    createOnepieceTable();
+    createLongDress();
+  }
+}//data 삽입
+function insertTopData() {
+  db.transaction(function(tx) {
+    tx.executeSql("insert into Top values(?,?,?,?,?)", [top_name.value, top_shoulder.value, top_chest.value, top_arm.value, top_height.value]);
+  });
+}
+
+function insertBottomData() {
+  db.transaction(function(tx) {
+    tx.executeSql("insert into Bottom values(?,?,?,?,?,?)", [bottom_name.value, bottom_height.value, bottom_waist.value, bottom_thigh.value, bottom_under.value, bottom_edge.value]);
+  });
+}
+
+function insertSkirtData() {
+  db.transaction(function(tx) {
+    tx.executeSql("insert into Skirt values(?,?,?,?)", [skirt_name.value, skirt_height.value, skirt_waist.value, skirt_edge.value]);
+  });
+}
+
+function insertOnepieceData() {
+  db.transaction(function(tx) {
+    tx.executeSql("insert into Onepiece values(?,?,?,?,?)", [onpiece_name.value, onepiece_shoulder.value, onepiece_Chest.value, onepiece_arm.value, onepiece_heigth.value]);
+  });
+}
+
+function insertLongDresseData() {
+  db.transaction(function(tx) {
+    tx.executeSql("insert into LongDress values(?,?,?,?,?,?,?)", [LongDress_name.value, LongDress_shoulder.value, LongDress_Chest.value, LongDress_arm.value, LongDress_height.value, LongDress_waist.value, LongDress_edge.value]);
+  });
+}
+
+//data 호출
+function selectTopData() {
+  db.transaction(function(tx) {
+    tx.executeSql("select * from Top", [],
+      function(tx, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          document.getElementById('table1').innerHTML += "<tr><td>" + row['top_name'] + "</td><td>" + row['top_shoulder'] + "</td><td>" + row['top_chest'] + "</td><td>" + row['top_arm'] + "</td><td>" + row['top_height'] + "</td></tr>";
+        }
+      });
+  });
+}
+
+function selectBottomData() {
+  db.transaction(function(tx) {
+    tx.executeSql("select * from Bottom", [],
+      function(tx, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          document.getElementById('table1').innerHTML += "<tr><td>" + row['bottom_name'] + "</td><td>" + row['bottom_height'] + "</td><td>" + row['bottom_waist'] + "</td><td>" + row['bottom_thigh'] + "</td><td>" + row['bottom_under'] + "</td><td>" + row['bottom_edge'] + "</td></tr>";
+        }
+      });
+  });
+}
+
+function selectSkirtData() {
+  db.transaction(function(tx) {
+    tx.executeSql("select * from Skirt", [],
+      function(tx, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          document.getElementById('table1').innerHTML += "<tr><td>" + row['skirt_name'] + "</td><td>" + row['skirt_height'] + "</td><td>" + row['skirt_waist'] + "</td><td>" + row['skirt_edge'] + "</td></tr>";
+        }
+      });
+  });
+}
+
+function selectOnepieceData() {
+  db.transaction(function(tx) {
+    tx.executeSql("select * from Onepiece", [],
+      function(tx, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          document.getElementById('table1').innerHTML += "<tr><td>" + row['onepiece_name'] + "</td><td>" + row['onepiece_shoulder'] + "</td><td>" + row['onepiece_Chest'] + "</td><td>" + row['onepiece_arm'] + "</td><td>" + row['onepiece_heigth'] + "</td></tr>";
+        }
+      });
+  });
+}
+
+function selectOnepieceData() {
+  db.transaction(function(tx) {
+    tx.executeSql("select * from Onepiece", [],
+      function(tx, result) {
+        for (var i = 0; i < result.rows.length; i++) {
+          var row = result.rows.item(i);
+          document.getElementById('table1').innerHTML += "<tr><td>" + row['onepiece_name'] + "</td><td>" + row['onepiece_shoulder'] + "</td><td>" + row['onepiece_Chest'] + "</td><td>" + row['onepiece_arm'] + "</td><td>" + row['onepiece_heigth'] + "</td></tr>";
+        }
+      });
+  });
+}
